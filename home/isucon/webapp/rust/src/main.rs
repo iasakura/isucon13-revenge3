25fd1d0a2babbd321d86f8926f51514b3914968c
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2024-01-17 02:24:27 +0900

    a

--- home/isucon/webapp/rust/src/main.rs
+++ home/isucon/webapp/rust/src/main.rs
@@ -1735,13 +1735,16 @@ async fn verify_user_session(jar: &SignedCookieJar) -> Result<(), Error> {
     let cookie = jar
         .get(DEFAULT_SESSION_ID_KEY)
         .ok_or(Error::Forbidden("".into()))?;
+    tracing::info!("a");
     let sess = CookieStore::new()
         .load_session(cookie.value().to_owned())
         .await?
         .ok_or(Error::Forbidden("".into()))?;
+    tracing::info!("b");
     let session_expires: i64 = sess
         .get(DEFUALT_SESSION_EXPIRES_KEY)
         .ok_or(Error::Forbidden("".into()))?;
+    tracing::info!("c");
     let now = Utc::now();
     if now.timestamp() > session_expires {
         return Err(Error::Unauthorized("session has expired".into()));
